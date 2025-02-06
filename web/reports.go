812830eef3ac84f368cb937db179fff99a32e7cf
standoffvenus <liam.mueller315@gmail.com>
2025-02-05 23:26:41 -0600

    fix: reduce match shortnames to numbers

--- web/reports.go
+++ web/reports.go
@@ -12,7 +12,9 @@ import (
 	"math"
 	"net/http"
 	"strconv"
+	"strings"
 	"time"
+	"unicode"
 
 	"github.com/Team254/cheesy-arena/game"
 	"github.com/Team254/cheesy-arena/model"
@@ -20,6 +22,20 @@ import (
 	"github.com/Team254/cheesy-arena/tournament"
 	"github.com/jung-kurt/gofpdf"
 )
+
+// ExtractNumbersFromString will return just the numbers in the provided string;
+// the returned number-only string will be in the same order it was passed, e.g.
+//
+//	ExtractNumbersFromString("a7b3c04") // => "7304"
+func ExtractNumbersFromString(s string) string {
+	return strings.Map(func(r rune) rune {
+		if unicode.IsDigit(r) {
+			return r
+		}
+
+		return -1
+	}, s)
+}
 
 // Generates a CSV-formatted report of the qualification rankings.
 func (web *Web) rankingsCsvReportHandler(w http.ResponseWriter, r *http.Request) {
@@ -397,6 +413,10 @@ func (web *Web) scheduleNexusCsvReportHandler(w http.ResponseWriter, r *http.Req
 	if err != nil {
 		handleWebErr(w, err)
 		return
+	}
+
+	for index, match := range matches {
+		matches[index].ShortName = ExtractNumbersFromString(match.ShortName)
 	}
 
 	// Don't set the content type as "text/csv", as that will trigger an automatic download in the browser.
